50fc21193611c440c4b600482e1be06001ca1dbf
nsj3 <Stephen.Juggins@ncl.ac.uk>
2022-10-27 19:25:49 +0100

    Version 1.04

--- R/zzz.r
+++ R/zzz.r
@@ -1,7 +1,6 @@
 .onAttach <- function(lib, pkg)  {
    line1 <- paste("This is rioja ", utils::packageDescription("rioja", fields="Version"))
-   line2 <- "New function riojaPlot replaces strat.plot for plotting stratigraphic diagrams\nSee ?riojaPlot and vignette(\"riojaPlot\") or vignette(\"riojaPlotPDF\") for details."
-   packageStartupMessage(paste(line1, line2, sep="\n"), appendLF = TRUE)
+   packageStartupMessage(paste(line1), appendLF = TRUE)
 }
 
 .onUnload <- function(libpath) {
